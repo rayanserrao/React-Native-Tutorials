9f33971a71ecb02ec03cca30994d8f613557aed3
rayanserrao <rayanserrao25@gmail.com>
2021-05-28 00:17:47 +0530

    Reducers and validation using reducers

--- rn-starter/src/screens/squarescreen.js
+++ rn-starter/src/screens/squarescreen.js
@@ -1,48 +1,63 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, Button, StyleSheet, View } from 'react-native';
 import { color } from 'react-native-reanimated';
 import ColorCounter from '../components/colorcounter'
 
-const sqaureScreen = () => {
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
-    const COLOR_VALUE = 15;
 
-    const setColor = (color, change) => {
+const COLOR_VALUE = 15;
 
-        switch (color) {
-            case 'red':
-                red + change > 255 || red + change < 0 ? null : setRed(red + change)
-                return;
-        }       
 
-        switch (color) {
-            case 'green':
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-                return;
-        }
+const reducer = (state, action) => {
+    //     state - {red:Number, green:number, blue:nimber}
+    //     action - {colorrochnage :red/green/blue, amount:=15/-15}
+    switch (action.colorToChange) {
+        case 'red':
+           return  state.red+action.amount >255 || state.red +action.amount<0 ? state
+             :{ ...state, red: state.red + action.amount };
+        // ...state - {red:0, green:0 and blue:0 at initail steps}
+        //action.amount - +15/-15
+        //state.red - 0 at inital step
+        case 'green':
+            return state.green+action.amount >255 || state.green +action.amount<0 ? state
+            : { ...state, green: state.green + action.amount };
 
-        switch (color) {
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-                return;
-        }
+        case 'blue':
+            return state.blue+action.amount >255 || state.blue +action.amount<0 ? state
+            : { ...state, blue: state.blue + action.amount };
+
+        default:
+            return state;
     }
+
+
+
+};
+
+
+
+const sqaureScreen = () => {
+
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+    const {red, green, blue} = state;
+
+
+
+
+
 
     return (
         <View>
             <ColorCounter
-                onIncrease={() => setColor('red', COLOR_VALUE)}
-                onDecrease={() => setColor('red', -1 * COLOR_VALUE)}
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_VALUE })}
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_VALUE })}
                 color='Red' />
             <ColorCounter
-                onDecrease={() => setColor('green',  COLOR_VALUE)}
-                onIncrease={() => setColor('green',-1* COLOR_VALUE)}
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_VALUE })}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_VALUE })}
                 color='Green' />
             <ColorCounter
-                onIncrease={() => setColor('blue',  COLOR_VALUE)}
-                onDecrease={() => setColor('blue',-1* COLOR_VALUE)}
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_VALUE })}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_VALUE })}
                 color='Blue' />
 
             <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }} ></View>
